8043134b8de89becfef1103e0295dfa360cf3343
Secret-SantaClaus <noel.secret.santaclaus@gmail.com>
2024-12-20 18:51:56 +0100

    Create index.mjs

---
+++ index.mjs
@@ -0,0 +1,40 @@
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://test-h23l.onrender.com/");
+xhr.send();
+
+import "./styles.css";
+
+let nom = [];
+let email = [];
+
+let button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+  let ajoutNom = document.getElementById("nm").value;
+  let ajoutEmail = document.getElementById("em").value;
+
+  if ((ajoutNom != "") & (ajoutEmail != "")) {
+    alert("Bravo !");
+    nom.push(ajoutNom);
+    document.getElementById("nm").value = "";
+    email.push(ajoutEmail);
+    document.getElementById("em").value = "";
+
+    ///document.getElementById("participants").innerHTML +=
+    ///nom.at(-1) + "   " + email.at(-1) + "\n";
+
+    let nouvPart = document.createElement("pre");
+    nouvPart.innerHTML = nom.at(-1) + "   " + email.at(-1);
+    document.getElementById("col1").appendChild(nouvPart);
+  }
+});
+
+let pre = document.querySelector("pre");
+
+pre.addEventListener("click", function () {
+  alert("Bravo !");
+  nom.push(document.getElementById("nm").value);
+  document.getElementById("nm").value = "";
+  email.push(document.getElementById("em").value);
+  document.getElementById("em").value = "";
+});
