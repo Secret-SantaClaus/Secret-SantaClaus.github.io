773b5feae45d74527415e62f182d9620a550be63
Secret-SantaClaus <noel.secret.santaclaus@gmail.com>
2025-01-12 20:24:24 +0100

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -14,36 +14,33 @@ btnajt.addEventListener("click", function () {
     email.push(ajoutEmail);
     document.getElementById("em").value = "";
 
-    ///document.getElementById("participants").innerHTML +=
-    ///nom.at(-1) + "   " + email.at(-1) + "\n";
-
     let nouvPart = document.createElement("pre");
     nouvPart.innerHTML = nom.at(-1) + "   " + email.at(-1);
     document.getElementById("col1").appendChild(nouvPart);
+    
     nouvPart.addEventListener("click", function () {
       alert("Supprimer !");
-      nom.push(document.getElementById("nm").value);
-      document.getElementById("nm").value = "";
-      email.push(document.getElementById("em").value);
-      document.getElementById("em").value = "";
     });
   }
-});
-
-let pre = document.querySelector("pre");
-
-pre.addEventListener("click", function () {
-  alert("Bravo !");
-  nom.push(document.getElementById("nm").value);
-  document.getElementById("nm").value = "";
-  email.push(document.getElementById("em").value);
-  document.getElementById("em").value = "";
 });
 
 env = document.getElementById("btnenv");
 env.addEventListener("click", function () {
   alert("envoyer")
+        
+  urln = "nom=["
+  for i in nom:
+    urln += i+","
+  urln += "]"
+
+  urle = "email=["
+  for i in email:
+    urle += i
+  urle += "]"
+
+  url = urln + "&" + urle
+  
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://serveur-8nsb.onrender.com");
+  xhr.open("GET", "https://serveur-8nsb.onrender.com/?"+url);
   xhr.send();
 })
